0b163496e6c0fbfa8d3a9b554d5509a6ef66656e
SerG <serg.arnisha@gmail.com>
2025-06-06 14:08:24 +0200

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+
+
+import {HeaderComponent} from "@/features/header"
+
+function App() {
+  
+
+  return (
+    <>
+      <HeaderComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/features/header/index.tsx
@@ -0,0 +1,3 @@
+export function HeaderComponent(){
+    return <div>Header Component</div>
+}
