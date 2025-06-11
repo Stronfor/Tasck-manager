8c82aeddcfb498bfe88ef033555f3fe747ddaa78
SerG <serg.arnisha@gmail.com>
2025-06-11 15:03:52 +0200

    work with .env variables in dev/prodact/local locatioan

--- src/features/board/board.page.tsx
+++ src/features/board/board.page.tsx
@@ -1,7 +1,10 @@
+import { CONFIG } from "@/shared/model/config";
 import type { PathParams, ROUTES } from "@/shared/model/routes";
 import { useParams } from "react-router-dom";
 
 function BoardPage(){
+
+    console.log(CONFIG.API_BASE_URL)
 
     const params = useParams<PathParams[typeof ROUTES.BOARD]>()
 
