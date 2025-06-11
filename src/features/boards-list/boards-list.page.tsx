1db6daa0688dbc5fa687d368eb4c0fe139edf258
SerG <serg.arnisha@gmail.com>
2025-06-11 17:43:01 +0200

    Add request for boards | delete board, create bord

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,17 +1,55 @@
 import { rqClient } from "@/shared/api/instance";
 import { ROUTES } from "@/shared/model/routes";
+import { useQueryClient } from "@tanstack/react-query";
 import {Link, href} from "react-router-dom"
 
 function BoardListPage() {
-
+  const queryClient = useQueryClient();
 
   const boardsData = rqClient.useQuery("get", "/boards");
+
+  const createBoardMutation = rqClient.useMutation("post", "/boards", {
+    onSettled: async () => {
+      await queryClient.invalidateQueries(rqClient.queryOptions("get", "/boards"))
+    },
+  });
+  const deleteBoardMutation = rqClient.useMutation("delete", "/boards/{boardId}", {
+    onSettled: async () => {
+      await queryClient.invalidateQueries(rqClient.queryOptions("get", "/boards"))
+    },
+  });
 
   return (
     <div>
       <h1>Boards List Page</h1>
-      {boardsData.data?.list.map((board) => (
-        <Link key={board.id} to={href(ROUTES.BOARD, {boardId: board.id})}>{board.name}</Link>
+
+      <form onSubmit={(e)=> {
+        e.preventDefault();
+
+        const formData = new FormData(e.target as HTMLFormElement)
+        createBoardMutation.mutate({
+          body: {name: formData.get("name") as string}
+        })
+      }}>
+        <input type="text" name="name" />
+        <button
+          type="submit"
+          disabled={createBoardMutation.isPending}
+        >
+          Create board
+        </button>
+      </form>
+
+      {boardsData.data?.list.map(({id, name}) => (
+        <div key={id}>
+          <Link to={href(ROUTES.BOARD, {boardId: id})}>{name}</Link>
+          <button 
+            onClick={()=> deleteBoardMutation.mutate({params: {path: {boardId: id}}})}
+            disabled={deleteBoardMutation.isPending}
+          >
+            Delete
+          </button>
+        </div>
       ))}
     </div>
   )
